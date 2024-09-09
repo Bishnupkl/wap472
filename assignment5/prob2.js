1a30d0f31deb2378ab799ec12f2134668c386872
bishnu <bishnup212@gmail.com>
2024-09-09 14:58:20 -0500

    assignement 5 push

--- assignment5/prob2.js
+++ assignment5/prob2.js
@@ -1,12 +1,43 @@
-const isPrime = (n) => {
-    return new Promise((resolve, reject) => {
-        if (n < 2) return reject({prime: false});
-        for (let i = 2, s = Math.sqrt(n); i <= s; i++) {
-            if (n % i === 0) return reject({prime: false});
+const isPrime = async (n) => {
+    if (n < 2) {
+        return { prime: false };
+    }
+
+    for (let i = 2, s = Math.sqrt(n); i <= s; i++) {
+        if (n % i === 0) {
+            return { prime: false };
         }
-        resolve({prime: true});
-    });
+    }
+
+    return { prime: true };
 };
+
+// const isPrime = (n) => {
+//     return new Promise((resolve, reject) => {
+//         if (n < 2) return reject({prime: false});
+//         for (let i = 2, s = Math.sqrt(n); i <= s; i++) {
+//             if (n % i === 0) return reject({prime: false});
+//         }
+//         resolve({prime: true});
+//     });
+// };
+
+// // Test using Promises
+// (() => {
+//     console.log('start');
+//
+//     isPrime(7)
+//         .then(result => {
+//             console.log(result);
+//         })
+//         .catch(error => {
+//             console.error(error);
+//         })
+//         .finally(() => {
+//             console.log('end');
+//         });
+// })();
+
 
 // Test using async/await
 
